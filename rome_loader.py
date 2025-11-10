e7a1cb904b8b6587c403f0c65f6f7a0daeb32777
tomjoy75 <tomjoy75@gmail.com>
2025-11-10 11:08:32 +0100

    feat: implement basic load_csv() using csv.DictReader()

--- rome_loader.py
+++ rome_loader.py
@@ -1,3 +1,10 @@
+import csv
+
 def load_csv(path: str) -> list[dict]:
     """Load a ROME CSV file and return it as a list of dicts."""
-    raise NotImplementedError("load_csv not implemented yet")
+#    raise NotImplementedError("load_csv not implemented yet")
+    with open(path, mode='r', errors='ignore') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return list(reader)
+
+
